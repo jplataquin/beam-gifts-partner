f1d0e7fb003e768fab7791621d85075960258a51
jplataquin <jp.lataquin@gmail.com>
2022-09-05 19:17:09 +0800

    test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+
+            $table->string('primary_contact_person');
+            $table->string('primary_contact_no');
+            $table->string('primary_contact_person_position');
+
+            $table->string('secondary_contact_person');
+            $table->string('secondary_contact_no');
+            $table->string('secondary_contact_person_position');
+            
+            $table->bigInteger('brand_id');
+            $table->text('branch');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
