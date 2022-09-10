35d414fa55c6210526a58e636506dba31a4d6e0c
jplataquin <jp.lataquin@gmail.com>
2022-09-11 01:22:03 +0800

    test

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -3,5 +3,17 @@
 @section('content')
 <div class="container">
     <h1>Scan</h1>
+    <video id="vidEl"></video>
+    <script type="module">
+        import QrScanner from '/qr-scanner.min.js';
+
+        const vidEl = document.querySelector('#vidEl');
+
+        const qrScanner = new QrScanner(
+            vidEl,
+            result => console.log('decoded qr code:', result),
+            { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
+        );
+    </script>
 </div>
 @endsection
