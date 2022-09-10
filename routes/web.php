2ccfdcea6388d3c834aa66a2b99400bd7e17ff9b
jplataquin <jp.lataquin@gmail.com>
2022-09-11 01:17:21 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/scan', [App\Http\Controllers\HomeController::class, 'scan'])->name('scan');
+
+
+Route::get('qr-scanner.min.js', function(){
+
+    $response = Response::make(File::get(base_path('node_modules/qr-scanner/qr-scanner.min.js')), 200);
+    $response->header("Content-Type", 'text/javascript');
+
+    return $response;
+});
