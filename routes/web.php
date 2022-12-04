8c2ab86847941214eaed135368ff1b306ee2e9e4
jplataquin <jp.lataquin@gmail.com>
2022-12-04 18:16:18 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::middleware(['auth'])->group(function () {
 
 });
 
+
 Route::get('/qr-scanner.min.js', function(){
 
     $response = Response::make(File::get(base_path('node_modules/qr-scanner/qr-scanner.min.js')), 200);
