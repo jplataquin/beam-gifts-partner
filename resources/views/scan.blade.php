4d22bd958c306ab576002a6e814af283e14d9d78
jplataquin <jp.lataquin@gmail.com>
2022-12-06 07:48:54 +0800

    test

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -5,7 +5,7 @@
     <h1>Scan</h1>
     <div class="row">
         <div class="col-12 text-center">
-            <video id="vidEl"></video>
+            <video width="500px" id="vidEl"></video>
         </div>
     </div>
    
