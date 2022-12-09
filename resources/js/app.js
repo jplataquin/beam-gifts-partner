dd4118f057c759de776dd8db4efa8b2bdce80b55
jplataquin <jp.lataquin@gmail.com>
2022-12-09 09:37:58 +0800

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,97 @@
 import './bootstrap';
+
+window.util = {};
+
+window.util.$get = async (url,data) => {
+
+    let status = '';
+    
+    return fetch(url+'?'+ new URLSearchParams(data),
+    {
+        headers: {
+            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+            "Accept": "application/json"
+        },
+        method: "GET"
+    }).then((response) => {
+        
+        status = response.status;
+
+        if(response.status == 401){
+            return {
+                    status:-1,
+                    message:'Please sign in',
+                    data:{}
+            }
+        };
+
+        if(response.status == 500){
+
+            console.error(response);
+            return {
+                    status:0,
+                    message:'Something went wrong',
+                    data:{}
+            }
+        };
+
+        return response.json();
+    }).catch(e=>{
+
+        return {
+            status:0,
+            message:e.message,
+            data:{
+                httpStatus: status
+            }
+        }
+    });
+}
+
+window.util.$post = async (url,formData) => {
+
+    let status = '';
+
+    return fetch(url,
+    {
+        headers: {
+            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+            "Accept": "application/json",
+        },
+        body: formData  ?? {},
+        method: "POST"
+    }).then((response) => {
+       
+        status = response.status;
+        
+        if(status == 401){
+          
+            return {
+                    status:-1,
+                    message:'Please sign in',
+                    data:{}
+            }
+        };
+
+        if(status == 500){
+
+            console.error(response);
+            return {
+                    status:0,
+                    message:'Something went wrong',
+                    data:{}
+            }
+        };
+
+        return response.json();
+    }).catch(e=>{
+
+        return {
+            status:0,
+            message:e.message,
+            data:{
+                httpStatus: status
+            }
+        }
+    });
+}
