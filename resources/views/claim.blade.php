49c6006ce7ea4b0833697fc0a59c7bf20f071f09
jplataquin <jp.lataquin@gmail.com>
2022-12-10 15:32:36 +0800

    test

--- resources/views/claim.blade.php
+++ resources/views/claim.blade.php
@@ -10,10 +10,20 @@
         <div class="col-12 text-center">
             <img width="400px" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$data->item->photo['400px']}}"/>
         </div>
-        <div class="col-12 text-center mt-3">
-            <h3>{{$data->item->name}}</h3>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-12 text-center">
+            <h4>{{$data->item_name}}</h4>
+            <h3>By</h3>
+            <h3>{{$data->brand_name}}</h3>
         </div>
     </div>
+
+    <div class="row">
+        <h3>PHP: {{ number_format($data->price,2) }}
+    </div>
+    
 
 </div>
 
