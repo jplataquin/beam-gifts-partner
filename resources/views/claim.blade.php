5ed18f41ac10ebd68909b95496fcdb0f9d9161ff
jplataquin <jp.lataquin@gmail.com>
2022-12-10 15:23:56 +0800

    test

--- resources/views/claim.blade.php
+++ resources/views/claim.blade.php
@@ -8,7 +8,7 @@
     
     <div class="row">
         <div class="col-12 text-center">
-            <img src="storage/photos/item/400px/{{$data->photo['400px']}}"/>
+            <img src="storage/photos/item/400px/{{$data->item->photo['400px']}}"/>
         </div>
     </div>
 
