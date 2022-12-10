956fd939d7c1b78542b7badcb538b09bab8034b0
jplataquin <jp.lataquin@gmail.com>
2022-12-10 15:25:58 +0800

    test

--- resources/views/claim.blade.php
+++ resources/views/claim.blade.php
@@ -8,7 +8,10 @@
     
     <div class="row">
         <div class="col-12 text-center">
-            <img src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$data->item->photo['400px']}}"/>
+            <img width="400px" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$data->item->photo['400px']}}"/>
+        </div>
+        <div class="col-12 text-center">
+            <h3>{{$data->name}}</h3>
         </div>
     </div>
 
