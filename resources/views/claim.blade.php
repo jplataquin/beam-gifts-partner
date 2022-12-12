fb9594046d482c8a5d554430b558d18220cd0ee0
jplataquin <jp.lataquin@gmail.com>
2022-12-12 13:41:52 +0800

    test

--- resources/views/claim.blade.php
+++ resources/views/claim.blade.php
@@ -7,7 +7,7 @@
     <h1>{{$uid}}</h1>
     
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-9 text-center">
             <div class="card" width="100%">
                 <div class="card-header">
                     &nbsp;
