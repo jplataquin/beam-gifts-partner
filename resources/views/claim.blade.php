f080e844bb62469224116ce863fec37283367e17
jplataquin <jp.lataquin@gmail.com>
2022-12-12 15:03:36 +0800

    test

--- resources/views/claim.blade.php
+++ resources/views/claim.blade.php
@@ -39,7 +39,7 @@
 
 <script type="module">
     import {$q} from '/adarna.js';
-    import Prompt from '/attention/src/js/prompt.js';
+    import {Prompt} from '/attention/dist/attention.js';
 
     (async ()=>{
 
