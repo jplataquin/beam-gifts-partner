88e5f602ce3e57cc44e695418bbd2e564897c5b8
jplataquin <jp.lataquin@gmail.com>
2022-12-12 13:34:09 +0800

    test

--- resources/views/claim.blade.php
+++ resources/views/claim.blade.php
@@ -7,24 +7,22 @@
     <h1>{{$uid}}</h1>
     
     <div class="row">
-        <div class="col-12 text-center">
-            <img width="400px" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$data->item->photo['400px']}}"/>
+        <div class="col-md-9">
+            <div class="card" width="100%">
+                <div class="card-header">
+                    &nbsp;
+                </div>
+
+                <img class="card-img-top" src="{{config('app')['api_base_url']}}storage/photos/item/400px/{{$data->item->photo['400px']}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{$data->item_name}}</h5>
+                    <p>By {{$data->b</p>
+                    <h3>PHP: {{ number_format($data->price,2) }}
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="row mt-3">
-        <div class="col-12 text-center">
-            <h2>{{$data->item_name}}</h2>
-            <h4>By</h4>
-            <h4>{{$data->brand_name}}</h4>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12 text-center">
-            <h3>PHP: {{ number_format($data->price,2) }}
-        </div>
-    </div>
 
 
 </div>
