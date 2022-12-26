fa9537fbb0228935e88876fdd0087a540cdddbc3
jplataquin <jp.lataquin@gmail.com>
2022-12-26 23:43:45 +0800

    test

--- app/Models/PartnerLog.php
+++ app/Models/PartnerLog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ParnterLog extends Model
+class PartnerLog extends Model
 {
     protected $table        = 'partner_log';
     use HasFactory;
