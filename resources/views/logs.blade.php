85158b4164a42feaf22b73ebfc4d7a86e8ceb7de
jplataquin <jp.lataquin@gmail.com>
2023-01-20 18:15:13 +0800

    test

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -43,7 +43,7 @@
                 }
 
                 
-
+                console.log(list,'asdasd');
                 reply.data.items.map(row => {
                     
                     let div = t.div(()=>{
