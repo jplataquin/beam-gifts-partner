f8df475dff300fc89f497779053ac0aa19ae6b57
jplataquin <jp.lataquin@gmail.com>
2023-01-23 13:51:08 +0800

    test

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -53,13 +53,15 @@
                     
                     let div = t.div({class:'card mb-3'},()=>{
 
-                        t.div({class:'card-header'},'&nbsp');
+                        t.div({class:'card-header'},' ');
                         
                         t.div({class:'card-body'},()=>{
                             t.h5({class:'card-title'},row.entry.item_name);
                             t.p({class:'card-text'},()=>{
                                 t.txt(util.numFormat.money('PHP',row.amount));
+                                t.br();
                                 t.txt(statusOpt[row.status]);
+                                t.txt(row.created_at);
                             });
                         });
                     });
